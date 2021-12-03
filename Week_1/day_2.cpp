3a89ef6a3d9a868cc48b6643c52711eae46cf4a9
Kit Gallagher <gallagherkit@outlook.com>
2021-12-03 19:30:47 +0000

    Added aim capability

--- Week_1/day_2.cpp
+++ Week_1/day_2.cpp
@@ -11,27 +11,42 @@
 #include <sstream>
 using namespace std;
 
-vector<int> update_pos(string command, int magnitude, vector<int> pos)
+vector<int> update_pos(string command, int magnitude, vector<int> pos, bool use_aim)
 {
     string up = "up"; // To avoid comparison to an absolute string
     string down = "down";
     string forward = "forward";
-    if (command == up){
-        pos[0] = pos[0] - magnitude;
+    if (use_aim){
+        if (command == up){
+            pos[2] = pos[2] - magnitude;
+        }
+        if (command == down){
+            pos[2] = pos[2] + magnitude;
+        }
+        if (command == forward){
+            pos[1] = pos[1] + magnitude;
+            pos[0] = pos[0] + magnitude * pos[2];
+        }
     }
-    if (command == down){
-        pos[0] = pos[0] + magnitude;
+    else {
+        if (command == up){
+            pos[0] = pos[0] - magnitude;
+        }
+        if (command == down){
+            pos[0] = pos[0] + magnitude;
+        }
+        if (command == forward){
+            pos[1] = pos[1] + magnitude;
+        }
     }
-    if (command == forward){
-        pos[1] = pos[1] + magnitude;
-    }
+
     return pos;
 }
 
-
 int main()
 {
-    std::vector<int> my_pos = {0, 0}; // 2D array in form [depth, x position] to define current position
+    std::vector<int> my_pos = {0, 0, 0}; // 2D array in form [depth, x position, aim] to define current position
+    bool USE_AIM = true;
 
     ifstream MyReadFile("Week_1/day_2_input.txt"); // Read from the text file
     string line; // Create a text string, which is used to output the text file
@@ -45,12 +60,13 @@ int main()
             tokens.push_back(buf);
 
         // First token is commmand, second token is distance/magnitude
-        my_pos = update_pos(tokens[0], stoi(tokens[1]), my_pos);
+        my_pos = update_pos(tokens[0], stoi(tokens[1]), my_pos, USE_AIM);
     }
 
     MyReadFile.close(); // Close the file
 
     cout << "Final Position: (" << my_pos[0] << ", " << my_pos[1] << ")" << endl;
+    cout << "Prolbem Solution: " << my_pos[0] * my_pos[1] << endl;
     return 0;
 }
 
