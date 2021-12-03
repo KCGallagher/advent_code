902f875f2d1701a7df3298905db84208877d123e
Kit Gallagher <gallagherkit@outlook.com>
2021-12-03 15:57:37 +0000

    Route Plotter challenge

---
+++ Week_1/day_2.cpp
@@ -0,0 +1,56 @@
+/* 
+ * Takes an input txt files with sequential instruction on where to travel
+ * Returns the final position attained by following these instructions
+ * Note that this is in the form [depth, x position] to define current position
+ */
+
+#include <iostream> 
+#include <fstream> 
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+vector<int> update_pos(string command, int magnitude, vector<int> pos)
+{
+    string up = "up"; // To avoid comparison to an absolute string
+    string down = "down";
+    string forward = "forward";
+    if (command == up){
+        pos[0] = pos[0] - magnitude;
+    }
+    if (command == down){
+        pos[0] = pos[0] + magnitude;
+    }
+    if (command == forward){
+        pos[1] = pos[1] + magnitude;
+    }
+    return pos;
+}
+
+
+int main()
+{
+    std::vector<int> my_pos = {0, 0}; // 2D array in form [depth, x position] to define current position
+
+    ifstream MyReadFile("Week_1/day_2_input.txt"); // Read from the text file
+    string line; // Create a text string, which is used to output the text file
+
+    // Use a while loop together with the getline() function to read the file line by line
+    while (getline (MyReadFile, line)) {
+        string buf;                 // Have a buffer string
+        stringstream ss(line);       // Insert the string into a stream
+        std::vector<std::string> tokens; // Create vector to hold our words
+        while (ss >> buf)
+            tokens.push_back(buf);
+
+        // First token is commmand, second token is distance/magnitude
+        my_pos = update_pos(tokens[0], stoi(tokens[1]), my_pos);
+    }
+
+    MyReadFile.close(); // Close the file
+
+    cout << "Final Position: (" << my_pos[0] << ", " << my_pos[1] << ")" << endl;
+    return 0;
+}
+
