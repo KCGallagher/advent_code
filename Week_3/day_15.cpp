a8eba0342ecc0b140a1381852814c0cfc2e8f097
Kit Gallagher <gallagherkit@outlook.com>
2021-12-15 23:00:59 +0000

    Devilish Dijkstra Determining Directions (day 15)

--- Week_3/day_15.cpp
+++ Week_3/day_15.cpp
@@ -1,7 +1,8 @@
 /* 
  * Takes an input txt files with an array of risks
  * Identifies a route through array with minimal risk using Dijkstra's algorithm
- * Returns the summed risk of the minimal risk route
+ * Returns the summed risk of the minimal risk route (ie the total path length)
+ * Also considers and tiled and clock stepped version of the array to find a path for.
  */
 
 #include <iostream> 
@@ -25,8 +26,60 @@ void print_2D_vector(vector< vector<int> > data)
     }
 }
 
+vector<int> vector_add(vector<int> data, int value){
+    // Adds a scalar to a vector element-wise
+    for (int n = 0; n < data.size(); n++){
+        data[n] += value;
+        if (data[n] > 9){
+            data[n] -= 9; // wrap back around
+        }
+    }
+    return data;
+}
+
+vector<vector<int>> tile_map(vector<vector<int>> data, int n){
+    vector<vector<int>> t_data;
+    for (int i = 0; i < n * data.size(); i++){
+        vector<int> row;
+        for (int m = 0; m < n; m++){ // Add line n times, with increment
+            // cout << i << " & " << m << endl;
+            vector<int> values = vector_add(data[i % data.size()], (i / data.size()) + m);
+            row.insert(row.end(), values.begin(), values.end());
+        }
+        t_data.push_back(row);
+
+    }
+    return t_data;
+}
+
+vector<vector<int>> pad_map(vector<vector<int>> data, int value){
+    vector<vector<int>> p_data;
+
+    // Start with frame row on top 
+    vector<int> frame_vector;
+    for (int x = 0; x < data[0].size() + 2; x++){ // + 2 for extra column on each side
+        frame_vector.push_back(value); 
+    }
+    p_data.push_back(frame_vector);
+    for (int i = 0; i < data.size(); i++){
+        vector<int> row_vector;
+        row_vector.push_back(value); // For left column of values
+        for (int j = 0; j < data[0].size(); j++){
+            row_vector.push_back(data[i][j]); 
+        }
+        row_vector.push_back(value); // For right column of  values
+        p_data.push_back(row_vector);
+    }
+    // Append row of large values on bottom of array too
+    p_data.push_back(frame_vector);
+    return p_data;
+}
+
+
+
 int update_distance(vector<vector<int>>& data, vector<vector<int>> risks, vector<vector<int>> visited, pair<int, int> index, int value)
 {
+    // Passing the whole arrays here is very inefficient, but made the indexing look nicer 
     if (visited[index.first][index.second]){
         return risks[index.first][index.second];  // Unchanged as visited
     } else{
@@ -99,7 +152,8 @@ int shortest_path(vector< vector<int> > data)
 int main()
 {
     // IMPORT ALL HEIGHT VALUES INTO ONE ARRAY
-    vector< vector<int> > risks; // Record height at each point in array
+    vector<vector<int>> risks; // Record height at each point in array
+    const int N_TILE = 5;
 
     ifstream MyReadFile("Week_3/Inputs/day_15_input.txt"); 
     string line; 
@@ -111,35 +165,22 @@ int main()
     // Half the max value of an integer is used - this is larger than any risks in the path, but won't overflow itself
 
     while (getline (MyReadFile, line)) {
-        if(line_num == 0){
-            int line_length = line.size(); // may be useful later
-            vector<int> frame_vector;
-            for (int i = 0; i < line.size() + 2; i++){
-                frame_vector.push_back(10); // - '0' converts char to int
-            }
-            risks.push_back(frame_vector);
-        }
         vector<int> row_vector;
-        row_vector.push_back(10); // For left column of values
         for (int i = 0; i < line.size(); i++){
             row_vector.push_back(line[i] - '0'); // - '0' converts char to int
         }
-        row_vector.push_back(10); // For right column of large values
         risks.push_back(row_vector);
-
-        line_num += 1;
     }
     MyReadFile.close(); // Close the file
 
-    // Append row of large values on bottom of array too
-    vector<int> frame_vector;
-    for (int i = 0; i < risks[0].size(); i++){
-        frame_vector.push_back(10); 
-    }
-    risks.push_back(frame_vector);
+    vector<vector<int>> p_risks = pad_map(risks, 10);
 
-    int total_risk = shortest_path(risks);
+    int total_risk_a = shortest_path(p_risks);
+    std::cout << "Final summed risk level for partial map: " << total_risk_a << endl;
 
-    std::cout << "Final summed risk level: " << total_risk << endl;
+    vector<vector<int>> full_risks = tile_map(risks, N_TILE); // required for part b
+    vector<vector<int>> p_full_risks = pad_map(full_risks, 10);
+    int total_risk_b = shortest_path(p_full_risks);
+    std::cout << "Final summed risk level for complete map: " << total_risk_b << endl;
     return 0;
 }
